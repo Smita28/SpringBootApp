448a268c85ebfe4aec7c2f8c18ef7e9f40908d92
Smita28 <smitadevi7@gmail.com>
2020-11-12 13:32:32 +0530

    Update EmployeDataCrudApplication.java
    added swagger bean

--- src/main/java/com/poc/project/EmployeDataCrud/EmployeDataCrudApplication.java
+++ src/main/java/com/poc/project/EmployeDataCrud/EmployeDataCrudApplication.java
@@ -22,5 +22,18 @@ public class EmployeDataCrudApplication {
 	      return new Docket(DocumentationType.SWAGGER_2).select()
 	         .apis(RequestHandlerSelectors.basePackage("com.poc.project.EmployeDataCrud")).build();
 	   }
+	 @Bean
+	    public WebMvcConfigurer webMvcConfigurer()
+	    {
+	        return new WebMvcConfigurer()
+	        {
+	            @Override
+	            public void addResourceHandlers( ResourceHandlerRegistry registry )
+	            {
+	                registry.addResourceHandler( "swagger-ui.html" ).addResourceLocations( "classpath:/META-INF/resources/" );
+	                registry.addResourceHandler( "/webjars/**" ).addResourceLocations( "classpath:/META-INF/resources/webjars/" );
+	            }
+	        };
+	    }
 
 }
